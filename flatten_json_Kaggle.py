544361c0274b1510e7fc2e3c2230c4fd1311af45
EddyGiusepe <eddychirinos.unac@gmail.com>
2021-12-15 18:17:22 -0300

    Finalizando nosso Script

--- flatten_json_Kaggle.py
+++ flatten_json_Kaggle.py
@@ -25,8 +25,58 @@ Vemos (pelo menos) duas colunas aninhadas, concerts e works. A documentação do
 dicas de como nivelar ainda mais os dados semiestruturados. Vamos desempacotar a coluna works em um dataframe
 independente. Também pegaremos as colunas planas para que possamos fazer análises. Os parâmetros aqui são um
 pouco heterodoxos, veja se você consegue entender o que está acontecendo.
+
 '''
 works_data = json_normalize(data=json_datos['programs'], record_path='works',
                             meta=['id', 'orchestra','programID', 'season'])
 print(works_data.head(3))
 print(works_data.shape)
+
+'''
+Excelente!
+
+* passou o caminho de dados do objeto json d[programs]
+* passou o caminho do record dentro do objeto que queríamos parsear funciona
+* passou os metadados pai que queríamos anexar
+
+Sua vez: você pode descompactar os dados dos concerts?
+'''
+
+'''
+Dados profundamente aninhados
+
+E daí se você encontrar uma matriz aninhada dentro da sua matriz aninhada? Se você voltar e olhar para works_data achatados,
+você pode ver uma segunda coluna aninhada, "soloists". Felizmente, os documentos json_normalize mostram que você pode passar
+uma lista de colunas, em vez de uma única coluna, para o caminho do record para unflatten json profundamente aninhado.
+
+Vamos flatten (achatar) os dados de 'soloists' aqui, passando uma lista. Uma vez que os soloists estão aninhados em works, podemos passar isso como:
+'''
+
+soloist_data = json_normalize(data=json_datos['programs'], record_path=['works', 'soloists'],
+                              meta=['id'])
+print(soloist_data.head(3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
